c98ff3da78774fbe40bbb1752e54d9561fb655bf
dexhoang <dhhoang@ucsc.eedu>
2024-11-07 19:31:55 -0800

    Add button to display alert message on click

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 // todo
+const button = document.createElement("button");
+button.innerHTML = "Button";
+document.body.append(button);
+
+button.addEventListener("click", () => {
+  alert("you clicked the button");
+});
